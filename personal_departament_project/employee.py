b5c84e6dd6348332ea3a2392254422bab19c75e9
a05314158 <151594394+a05314158@users.noreply.github.com>
2023-12-19 09:12:38 +0700

    Add files via upload
    employee file creates an employee database

---
+++ personal_departament_project/employee.py
@@ -0,0 +1,20 @@
+# employee.py
+
+class Employee:
+    def __init__(self, first_name, last_name, middle_name, phone_number, email, address, position):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.middle_name = middle_name
+        self.phone_number = phone_number
+        self.email = email
+        self.address = address
+        self.position = position
+
+    def display_info(self):
+        print(f"First Name: {self.first_name}")
+        print(f"Last Name: {self.last_name}")
+        print(f"Middle Name: {self.middle_name}")
+        print(f"Phone Number: {self.phone_number}")
+        print(f"Email: {self.email}")
+        print(f"Address: {self.address}")
+        print(f"Position: {self.position}")
