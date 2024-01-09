d2d3db99f10dd8e57747883d7bd78cc15ca971dc
a05314158 <151594394+a05314158@users.noreply.github.com>
2024-01-09 23:08:25 +0700

    Update employee_manager.py
    I've decided to remove the bot and go back to one of the first versions, but improved it.

--- personal_departament_project/employee_manager.py
+++ personal_departament_project/employee_manager.py
@@ -1,21 +1,12 @@
-import json
 from employee import Employee
 
 def display_employee_list(func):
     def wrapper(manager, *args, **kwargs):
         employees = manager.employees
         if employees:
-            print("\nEmployee List:")
-            print("*" * 94)
-            print("| {:<4} | {:<15} | {:<15} | {:<15} | {:<20} | {:<20} |".format("ID", "First Name", "Last Name", "Position", "Phone Number", "Email"))
-            print("*" * 94)
-            for i, employee in enumerate(employees, start=1):
-                print("| {:<4} | {:<15} | {:<15} | {:<15} | {:<20} | {:<20} |".format(i, employee.first_name, employee.last_name, employee.position, employee.phone_number, employee.email))
-            print("*" * 94)
+            func(manager, *args, **kwargs)
         else:
             print("No employees in the list.")
-        return func(manager, *args, **kwargs)
-
     return wrapper
 
 class EmployeeManager:
@@ -24,25 +15,26 @@ class EmployeeManager:
         self.employees = self.load_data()
 
     def load_data(self):
+        employees = []
         try:
             with open(self.file_path, mode='r', encoding='utf-8') as file:
-                employees_data = json.load(file)
-                employees = [Employee(**data) for data in employees_data]
-        except (FileNotFoundError, json.JSONDecodeError):
-            employees = []
+                for line in file:
+                    data = line.strip().split(',')
+                    employee = Employee(*data)
+                    employees.append(employee)
+        except FileNotFoundError:
+            print("File not found. Creating a new one.")
         return employees
 
     def save_data(self):
-        employees_data = [{"id": employee.id, "first_name": employee.first_name, "last_name": employee.last_name,
-                           "middle_name": employee.middle_name, "phone_number": employee.phone_number,
-                           "email": employee.email, "address": employee.address, "position": employee.position}
-                          for employee in self.employees]
-
         with open(self.file_path, mode='w', encoding='utf-8') as file:
-            json.dump(employees_data, file, indent=2)
+            for employee in self.employees:
+                data = [employee.first_name, employee.last_name, employee.middle_name,
+                        employee.phone_number, employee.email, employee.address, employee.position]
+                line = ','.join(data) + '\n'
+                file.write(line)
 
     def add_employee(self, employee):
-        employee.id = len(self.employees) + 1  # Assign a unique ID
         self.employees.append(employee)
         self.save_data()
 
@@ -50,14 +42,33 @@ class EmployeeManager:
         self.employees.remove(employee)
         self.save_data()
 
-    def update_employee(self, old_employee, new_employee):
-        index = self.employees.index(old_employee)
-        self.employees[index] = new_employee
+    def update_employee_info(self, employee):
+        print(f"Updating information for {employee.first_name} {employee.last_name}")
+        employee.phone_number = input("Enter new phone number: ")
+        employee.email = input("Enter new email address: ")
+        employee.address = input("Enter new address: ")
+        employee.position = input("Enter new position: ")
         self.save_data()
 
     @display_employee_list
-    def display_all_employees(self):
-        pass
+    def sort_employees_by_last_name(self):
+        self.employees = sorted(self.employees, key=lambda x: x.last_name)
+        self.display_all_employees()
 
     def get_employee_by_index(self, index):
         return self.employees[index - 1] if 1 <= index <= len(self.employees) else None
+
+    @display_employee_list
+    def display_all_employees(self):
+        print("\nEmployee List:")
+        print("*" * 94)
+        print("| {:<4} | {:<15} | {:<15} | {:<15} | {:<20} | {:<20} |".format("ID", "First Name", "Last Name", "Position", "Phone Number", "Email"))
+        print("*" * 94)
+        for i, employee in enumerate(self.employees, start=1):
+            print("| {:<4} | {:<15} | {:<15} | {:<15} | {:<20} | {:<20} |".format(i, employee.first_name, employee.last_name, employee.position, employee.phone_number, employee.email))
+        print("*" * 94)
+
+    def get_employee_choice(self):
+        self.display_all_employees()
+        choice = input("Select an employee (enter the number): ")
+        return self.get_employee_by_index(int(choice))
